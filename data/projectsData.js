d25e7262da3631108212afd364e0a3a2d250161f
majickdave <majickdave@gmail.com>
2023-07-08 18:01:10 -0700

    added new content

---
+++ data/projectsData.js
@@ -0,0 +1,66 @@
+const projectsData = [
+  {
+    title: 'Predicting Rich Attributes in Real Estate Images​',
+    description: `Developed an advanced web crawler designed to harvest images and pertinent metadata 
+    from the latest property listings on Redfin.com. Leveraged the collected data to construct an efficient few-shot 
+    learning model, capable of predicting distinct property attributes.`,
+    imgSrc: '/static/images/real-estate.png',
+    href: 'https://medium.com/@davesamuel/predicting-rich-attributes-in-real-estate-images-using-fastai-6d9ab248f359',
+  },
+  {
+    title: 'Kickstarter: Predicting Success',
+    description: `In this project, we process and analyze a vast dataset 
+    comprising more than 300,000 Kickstarter projects collected from 2009 to 2016.`,
+    imgSrc: '/static/images/kickstarter.png',
+    href: 'https://www.kaggle.com/majickdave/kickstarter-success-classifier-0-685',
+  },
+  {
+    title: 'Spotify Analysis',
+    description: `An exploratory data analysis project that aims to understand the highest volume streaming music on Spotify in 2017.`,
+    imgSrc: '/static/images/spotify.jpeg',
+    href: 'https://www.kaggle.com/majickdave/spotify-2017-analysis',
+  },
+  {
+    title: 'CryptoBox',
+    description: `CryptoBox brings cryptocurrency to the cloud, and currently offers a Price Ticker & Currency Calculator that 
+    helps you manage your cryptocurrency. The calculator allows you to simulate trades & token exchanges.`,
+    imgSrc: '/static/images/cryptobox.jpeg',
+    href: 'https://cryptobox.netlify.com/',
+  },
+  {
+    title: 'MusicMind',
+    description: `MusicMind is a Music Messenger App and Social Network that allows users to connect to their personal 
+    music collection or music streaming service, such as their Spotify account. They can then stream songs, and curate and 
+    share music video messages to the network, or to other social media platforms. MusicMind has a Database that holds and 
+    collects lyrics and other types of music metadata suchch as artist popularity, and audio-analysis features. We are using 
+    music metadata, and lyrics to transform songs into visual augmentation, creating a multi-media experience for users to 
+    engage more with music in real-time.`,
+    imgSrc: '/static/images/musicmind.png',
+    href: 'https://github.com/majickdave/MusicMind-iOS',
+  },
+  {
+    title: 'Natural Language Processing using live Twitter data',
+    description: `This paper dives into the process of collecting and analyzing tweets from Twitter's public API. 
+    The project details the use of Spark, and nlp library to discover tweets, determine context, and filter topics. 
+    As a result of this project, my team was able to analyze thousands of tweets leading up to the 2016 NBA Finals to predict the winner, 
+    twitterThe Golden State Warriors.`,
+    imgSrc: '/static/images/twitter.png',
+    href: 'https://drive.google.com/file/d/0B1NVR7hT8qCOX2s3ZFZoUG9lVnc/view?usp=sharing',
+  },
+  {
+    title: 'Facial Image Emotional Analysis using Microsoft Cognitive Service',
+    description: `This paper was written with a team of peers from SMU in 2016. It covers data collection, 
+    analysis and reporting on how public images of the presidential candidates affected their polling numbers on a weekly basis.`,
+    imgSrc: '/static/images/facial.png',
+    href: 'https://drive.google.com/open?id=0B1NVR7hT8qCOdXk3ei10WkNiNjQ',
+  },
+  {
+    title: 'Survey of Bitcoin, Ethereum, and the future of blockchain',
+    description: `This paper was written by myself for a Network Security Class in 2016. 
+    It provides a survey of blockchain technology, and it's potential impact on banking, finance, privacy and democracy.`,
+    imgSrc: '/static/images/btc.jpeg',
+    href: 'https://drive.google.com/file/d/0B1NVR7hT8qCORHV6T1FrSXY5WHM/view',
+  },
+]
+
+export default projectsData
